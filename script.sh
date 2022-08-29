ae30267937a4d68799ff11b3234361cdaac3aa0d
anishka-patel <anishka.vpatel@gmail.com>
2022-08-29 11:49:33 +0530

    Fixed script.sh

--- script.sh
+++ script.sh
@@ -1,23 +1,31 @@
 #!/bin/bash
 echo "<<-------------->>"
+echo ""
 DATE=$(date +"%a     %d.%m.%y    %H:%M")
 echo "$DATE"
-echo "---------------------------------------"
+echo ""
 echo -n "$USER @ $HOSTNAME"
 echo "  <$(uname -r)>"
-echo "---------------------------------------"
-MEMTOTINFO=$(cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi|sed 's/ kB/G/g'| grep -i memtotal | awk '{print $2}' )
-MEMAVLINFO=$(cat /proc/meminfo | numfmt --field 2 --from-unit=Ki --to-unit=Gi|sed 's/ kB/G/g'| grep -i memavailable | awk '{print $2}'| numfmt --field 2 --from-unit=Ki --to-unit=Gi|sed 's/ kB/G/g')
-echo "MEM:   tot ${MEMTOTINFO}               idl ${MEMAVLINFO}" 
+echo ""
+echo ""
+MEMTOTINFO=$(cat /proc/meminfo | 
+  numfmt --field 2 --from-unit=Ki --to-unit=Gi |
+  sed 's/ kB//g'| grep -i memtotal | awk '{print $2}')
+MEMAVLINFO=$(cat /proc/meminfo |
+  numfmt --field 2 --from-unit=Ki --to-unit=Gi |
+  sed 's/ kB//g'| grep -i memavailable | awk '{print $2}')
+echo "MEM:   tot ${MEMTOTINFO}G   use $((MEMTOTINFO - MEMAVLINFO))G   idl ${MEMAVLINFO}G" 
 echo "---------------------------------------"
 CPUUSRINFO=$(mpstat 1 1 | grep -i average | awk '{print $3}')
 CPUSYSINFO=$(mpstat 1 1 | grep -i average | awk '{print $5}')
 CPUIDLINFO=$(mpstat 1 1 | grep -i average | awk '{print $12}')
-echo "CPU:   usr ${CPUUSRINFO}  sys ${CPUSYSINFO}  idl ${CPUIDLINFO}"
+echo "CPU:   usr ${CPUUSRINFO}   sys ${CPUSYSINFO}   idl ${CPUIDLINFO}"
 echo "---------------------------------------"
-PSUSR=$(ps --user ${USER} | wc -l)
+PSUSR=$(ps --user "$USER" | wc -l)
 PSSYS=$(ps --user root | wc -l)
-echo "PROC:  usr ${PSUSR}                sys ${PSSYS}"
+PSTOT=$(ps -ef | wc -l)
+echo "PRO:   tot ${PSTOT}   usr ${PSUSR}   sys ${PSSYS}"
+echo ""
 echo "---------------------------------------"
 PACMANPKGS=$(pacman -Q | wc -l)
 FLATPKGS=$(flatpak list | wc -l)
@@ -28,7 +36,8 @@ echo "         Flatpak : ${FLATPKGS}"
 echo -n "Brew    : ${BREWPKGS}"
 echo "           Nix     : ${NIXPKGS}"
 echo "---------------------------------------"
-echo "Shortcut Guide"
+echo ""
+echo "Shortcuts"
 echo "----------------"
 echo "---System---"
 echo "Launcher               Meta"
