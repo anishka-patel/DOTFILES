e7b89ec42ebdb9b716c3bbd409c18cfb22ad8b58
anishka-patel <anishka.vpatel@gmail.com>
2022-09-10 21:49:34 +0530

    Tweaked script.sh

--- script.sh
+++ script.sh
@@ -3,8 +3,15 @@
 # Created: 21-08-2022 
 # Last modified: 22-08-2022
 # Description: Script to run in Plasma Command Output Widget
+# Script is customised to cater to arch plasma setups
+# Please modify appropriately to cater to other setups
+# Dependencies : Resolution
+# - nerd-fonts : yay -S nerd-fonts-complete
+# - mpstat     : pacman -S sysstat
+# - iostat     : pacman -S iostat 
+# - ifstat     : pacman -S sysstat 
 
-DATE=$(date +" %d.%m.%y  %a   %H:%M")
+DATE=$(date +" %d.%m.%y  %a   %H:%M (%z)")
 echo "$DATE"
 echo ""
 echo -n "$USER @ $HOSTNAME"
@@ -16,12 +23,12 @@ MEMTOTINFO=$(cat /proc/meminfo |
 MEMAVLINFO=$(cat /proc/meminfo |
   numfmt --field 2 --from-unit=Ki --to-unit=Gi |
   sed 's/ kB//g'| grep -i memavailable | awk '{print $2}')
-echo "MEM:    ${MEMTOTINFO}G     $((MEMTOTINFO - MEMAVLINFO))G   累 ${MEMAVLINFO}G" 
+echo "MEM:     ${MEMTOTINFO}G     $((MEMTOTINFO - MEMAVLINFO))G   累 ${MEMAVLINFO}G" 
 echo "---------------------------------------"
 CPUUSRINFO=$(mpstat 1 1 | grep -i average | awk '{print $3}')
 CPUSYSINFO=$(mpstat 1 1 | grep -i average | awk '{print $5}')
 CPUIDLINFO=$(mpstat 1 1 | grep -i average | awk '{print $12}')
-echo "CPU:   ${CPUUSRINFO}    ${CPUSYSINFO} 累 ${CPUIDLINFO}"
+echo "CPU:    ${CPUUSRINFO}    ${CPUSYSINFO}  累 ${CPUIDLINFO}"
 echo "---------------------------------------"
 IONAME="nvme0n1"
 IOREADINFO=$(iostat | grep "$IONAME" | awk '{print $3}')
@@ -31,7 +38,7 @@ echo "---------------------------------------"
 PSUSR=$(ps --user "$USER" | wc -l)
 PSSYS=$(ps --user root | wc -l)
 PSTOT=$(ps -ef | wc -l)
-echo "PRO:    ${PSTOT}    ${PSUSR}     ${PSSYS}"
+echo "PRO:    ${PSTOT}    ${PSUSR}    ${PSSYS}"
 echo "---------------------------------------"
 IFNAME="wlp1s0"
 IFRXINFO=$(ifstat | grep "$IFNAME" | awk '{print $6}')
@@ -39,22 +46,22 @@ IFTXINFO=$(ifstat | grep "$IFNAME" | awk '{print $8}')
 echo "NET:  ${IFNAME}   ${IFRXINFO}   ${IFTXINFO}"
 echo ""
 echo "---------------------------------------"
-PACPKGS=$(pacman -Qn | wc -l)
+PACMANPKGS=$(pacman -Qn | wc -l)
 YAYPKGS=$(yay -Qm | wc -l)
-FLATPKGS=$(flatpak list | wc -l)
+FLATPAKPKGS=$(flatpak list | wc -l)
 BREWPKGS=$(brew list | wc -l)
-PIPPKGS=$(pip list | wc -l)
-NPMPKGS=$(npm -g list | wc -l)
-CARGOPKGS=$(cargo install --list | wc -l)
+PYTHONPKGS=$(pip list | wc -l)
+NODEPKGS=$(npm -g list | wc -l)
+RUSTPKGS=$(cargo install --list | wc -l)
 GOPKGS=$(go list ... | wc -l)
-echo -n "Pac    $(printf "%4d" "$PACPKGS")"
-echo "   Yay    $(printf "%4d" "$YAYPKGS")"
-echo -n "Brew   $(printf "%4d" "$BREWPKGS")"
-echo "   Flat   $(printf "%4d" "$FLATPKGS")"
-echo -n "Npm    $(printf "%4d" "$NPMPKGS")"
-echo "   Pip    $(printf "%4d" "$PIPPKGS")"
-echo -n "Go     $(printf "%4d" "$GOPKGS")"
-echo "   Rust   $(printf "%4d" "$CARGOPKGS")"
+echo -n "Pacman  $(printf "%5d" "$PACMANPKGS")"
+echo "   Yay     $(printf "%5d" "$YAYPKGS")"
+echo -n "Brew    $(printf "%5d" "$BREWPKGS")"
+echo "   Flatpak $(printf "%5d" "$FLATPAKPKGS")"
+echo -n "Node    $(printf "%5d" "$NODEPKGS")"
+echo "   Python  $(printf "%5d" "$PYTHONPKGS")"
+echo -n "Go      $(printf "%5d" "$GOPKGS")"
+echo "   Rust    $(printf "%5d" "$RUSTPKGS")"
 echo "---------------------------------------"
 echo ""
 echo ""
