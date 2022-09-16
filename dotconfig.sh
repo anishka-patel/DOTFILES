16e3dfe2c5dd0977c6bf605a33bbbc7cbcf2e908
anishka-patel <anishka.vpatel@gmail.com>
2022-09-16 22:23:19 +0530

    Modified READMEs

--- dotconfig.sh
+++ dotconfig.sh
@@ -2,9 +2,9 @@
 # Title: Dotconfig Script
 # Description: A script file to move relevant files to relevent directories and backup where it is required
 # Created: [2022-09-16 Fri]
-# usage: ./dotconfig.sh
+# Usage: ./dotconfig.sh
 
-if [[ -f ~/.bashrc]]; then
+if [[ -f ~/.bashrc ]]; then
     echo -n "Backing up old .bashrc file..."
     mv ~/.bashrc ~/.bashrc.bak
     echo "Completed"
@@ -13,16 +13,16 @@ echo -n "Setting up bash files..."
 mv .bashrc ~/.bashrc
 echo "Completed"
 
-if [[ -f ~/.zshrc]]; then
+if [[ -f ~/.zshrc ]]; then
     echo -n "Backing up old .zshrc file..."
     mv ~/.zshrc ~/.zshrc.bak
     echo "Completed"
 fi
-echo =n "Setting up zsh files..."
+echo -n "Setting up zsh files..."
 mv .zshrc ~/.zshrc
 echo "Completed"
 
-if [[ -f ~/.vimrc]]; then
+if [[ -f ~/.vimrc ]]; then
     echo -n "Backing up old vim files..."
     mv ~/.vimrc ~/.vimrc.bak
     echo "Completed"
@@ -31,7 +31,7 @@ echo -n "Setting up vim files..."
 mv .vimrc ~/.vimrc
 echo "Completed"
 
-if [[ -f ~/.config/nvim/init.lua]]; then
+if [[ -f ~/.config/nvim/init.lua ]]; then
     echo -n "Backing up old nvim/init.lua"
     mv ~/.config/nvim/init.lua ~/.config/nvim/init.lua.bak
     echo "Completed"
@@ -39,7 +39,7 @@ fi
 echo -n "Setting up nvim/init.lua"
 mv .config/nvim/init.lua ~/.config/nvim/init.lua
 echo "Completed"
-if [[ -f ~/.config/nvim/lua/user/init.lua]]; then
+if [[ -f ~/.config/nvim/lua/user/init.lua ]]; then
     echo -n "Backing up old nvim/lua/user/init.lua"
     mv ~/.config/nvim/lua/user/init.lua ~/.config/nvim/lua/user/init.lua.bak
     echo "Completed"
@@ -48,7 +48,7 @@ echo -n "Setting up nvim/lua/user/init.lua"
 mv .config/nvim/lua/user/init.lua ~/.config/nvim/lua/user/init.lua
 echo "Completed"
 
-if[[ -d ~/.doom.d]]; then
+if [[ -d ~/.doom.d/ ]]; then
     echo -n "Backing up old doom emacs files..."
     mv ~/.doom.d ~/.doom.d.bak
     echo "Completed"
@@ -60,7 +60,7 @@ echo "Completed"
 echo "Installing vscodium extensions..."
 cat packages-list/vscodium-packages-list.txt | xargs -L1 codium --install-extension
 echo "Installing vscodium extensions... Completed"
-if [[-d ~/.config/VSCodium/user]]; then
+if [[ -d ~/.config/VSCodium/user ]]; then
     echo -n "Backing old codium files"
     mv ~/.config/VSCodium/user ~/.config/VSCodium/user.bak
     echo "Completed"
