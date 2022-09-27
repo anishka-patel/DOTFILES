ef4be07fccf50600921cce7a101b695650086fbb
anishka-patel <anishka.vpatel@gmail.com>
2022-09-27 19:15:06 +0530

    Removed unused modules from .doom.d/init.el

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -36,22 +36,22 @@
        (emoji +unicode)    ; 🙂
        hl-todo             ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
        ;;hydra
-       indent-guides       ; highlighted indent columns
+       ;;indent-guides       ; highlighted indent columns
        (ligatures +fira)         ; ligatures and symbols to make your code pretty again
        ;;minimap           ; show a map of the code on the side
        modeline            ; snazzy, Atom-inspired modeline, plus API
-       nav-flash           ; blink cursor line after big motions
+       ;;nav-flash           ; blink cursor line after big motions
        neotree             ; a project drawer, like NERDTree for vim
        ophints             ; highlight the region an operation acts on
        (popup +defaults)   ; tame sudden yet inevitable temporary windows
-       tabs                ; a tab bar for Emacs
+       ;; tabs                ; a tab bar for Emacs
        ;;treemacs          ; a project drawer, like neotree but cooler
        unicode             ; extended unicode support for various languages
        vc-gutter           ; vcs diff in the fringe
        vi-tilde-fringe     ; fringe tildes to mark beyond EOB
-       (window-select
-        +numbers
-        +switch-window)    ; visually switch windows
+       ;; (window-select
+       ;;  +numbers
+       ;;  +switch-window)    ; visually switch windows
        workspaces          ; tab emulation, persistence & separate workspaces
        zen                 ; distraction-free coding or writing
 
@@ -62,12 +62,12 @@
        (format +onsave)    ; automated prettiness
        ;;god               ; run Emacs commands without modifier keys
        ;;lispy             ; vim for lisp, for people who don't like vim
-       multiple-cursors    ; editing in many places at once
+       ;;multiple-cursors    ; editing in many places at once
        ;;objed             ; text object editing for the innocent
        ;;parinfer          ; turn lisp into python, sort of
        ;;rotate-text       ; cycle region at point between text candidates
        snippets            ; my elves. They type so I don't have to
-       word-wrap           ; soft wrapping with language-aware indent
+       ;;word-wrap           ; soft wrapping with language-aware indent
 
        :Emacs
        (dired +icons)      ; making dired pretty [functional]
@@ -78,8 +78,8 @@
 
        :term
        eshell              ; the elisp shell that works everywhere
-       shell               ; simple shell REPL for Emacs
-       ;;term              ; basic terminal emulator for Emacs
+       ;;shell               ; simple shell REPL for Emacs
+       ;;term                ; basic terminal emulator for Emacs
        vterm               ; the best terminal emulation in Emacs
 
        :checkers
@@ -162,16 +162,12 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org
-        +brain
-        +journal
         +jupyter
-        +noter
         +pandoc
         +pomodoro
         +pretty
         +present
-        +publish
-        +roam2)            ; organize your plain life in plain text
+        +publish)            ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
