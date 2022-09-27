e12ece4fae0914caa18f418fd7b37b424b368051
anishka-patel <anishka.vpatel@gmail.com>
2022-09-27 19:21:43 +0530

    Fixed linespacing in init.lua for astronvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,9 +2,12 @@ if vim.g.vscode then
   -- VSCode extensijon
   local opts = { noremap = true, silent = true }
   local keymap = vim.api.nvim_set_keymap
+
   keymap("", "<Space>", "<Nop>", opts)
+
   vim.g.mapleader = " "
   vim.g.maplocalleader = " "
+
   keymap("n", "<leader>", ":call VSCodeNotify('whichkey.show')<CR>", opts)
   keymap("x", "<leader>", ":call VSCodeNotify('whichkey.show')<CR>", opts)
 
