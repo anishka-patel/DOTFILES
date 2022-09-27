889aa40f5fc3fa0cfa7f1baefbb20804f56f9baa
anishka-patel <anishka.vpatel@gmail.com>
2022-09-27 19:19:17 +0530

    Enabled doom-themes settings

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -26,6 +26,9 @@
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
 (setq doom-theme 'doom-dracula)
+(after! doom-themes
+  (setq doom-themes-enable-bold t)
+  (setq doom-themes-enable-italic t))
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
