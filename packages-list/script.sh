3702c6653cef16d73d99d266c4dad91d8bb05859
anishka-patel <anishka.vpatel@gmail.com>
2022-09-28 12:22:52 +0530

    Updated packages lists

--- packages-list/script.sh
+++ packages-list/script.sh
@@ -8,28 +8,28 @@
 
 echo "Starting Packages list generation script."
 echo 
-echo -n "Generating Pacman packages list..."
+echo -n "Generating Pacman   packages list..."
 pacman -Qn | awk '{print $1}' > pacman-packages-list.txt 
 echo "Completed"
-echo -n "Generating Yay packages list..."
+echo -n "Generating Yay      packages list..."
 yay -Qm | awk '{print $1}'> yay-packages-list.txt
 echo "Completed"
-echo -n "Generating Brew packages list..." 
+echo -n "Generating Brew     packages list..." 
 brew list > brew-packages-list.txt
 echo "Completed"
-echo -n "Generating Flatpak packages list..."
+echo -n "Generating Flatpak  packages list..."
 flatpak list | awk '{print $1}' > flatpak-packages-list.txt
 echo "Completed"
-echo -n "Generating Npm packages list..."
+echo -n "Generating Npm      packages list..."
 npm -g list | awk '{print $2}' | awk -F '@' '{print $1}' > npm-packages-list.txt
 echo "Completed"
-echo -n "Generating Pip packages list..."
+echo -n "Generating Pip      packages list..."
 pip list | awk '{print $1}' > pip-packages-list.txt
 echo "Completed"
-echo -n "Generating Go packages list..."
+echo -n "Generating Go       packages list..."
 go list ... > go-packages-list.txt
 echo "Completed"
-echo -n "Generating Cargo packages list..."
+echo -n "Generating Cargo    packages list..."
 cargo install --list > cargo-packages-list.txt
 echo "Completed"
 echo -n "Generating VSCodium packages list..."
